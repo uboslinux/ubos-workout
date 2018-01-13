daef4b890ca79a32568e5598ede375e97eef5a44
Johannes Ernst <jernst@indiecomputing.com>
2018-01-13 05:09:17 +0000

    Cleaning up license comments Changed license to AGPL3

--- workout-mysql1/create.sql
+++ workout-mysql1/create.sql
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2014 and later, Indie Computing Corp. All rights reserved. License: see package.
+ */
+
 CREATE TABLE `happenings` (
     ts    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     event VARCHAR(1024)
